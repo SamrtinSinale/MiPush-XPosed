f13f7094ebad6f06545f28284631dfeda3ac7eed
SamrtinSinale <sartsine@gmail.com>
2026-07-15 16:23:54 +0800

    Revert HookHMS to original (remove stale BackgroundActivity call)

--- xposed/src/main/java/one/yufz/hmspush/hook/hms/HookHMS.kt
+++ xposed/src/main/java/one/yufz/hmspush/hook/hms/HookHMS.kt
@@ -1,13 +1,7 @@
 package one.yufz.hmspush.hook.hms
 
 class HookHMS {
-    companion object {
-        private const val TAG = "HookHMS"
-    }
-
     fun hook(classLoader: ClassLoader) {
-        HookBackgroundActivityStart.hook(classLoader)
-
         if (HookPushNC.canHook(classLoader)) {
             HookPushNC.hook(classLoader)
         }
